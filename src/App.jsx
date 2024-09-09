3c1ce3d35d6dd507b3cad95b7b579679237a6129
Unrealrojo234 <otienoryan812@gmail.com>
2024-09-09 13:47:08 +0300

    First time commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { Html5Qrcode, Html5QrcodeScanner } from "html5-qrcode";
+
+export default function App() {
+  const [scanResult, setScanResult] = useState("");
+  const [show, setShow] = useState('none');
+
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner("reader", {
+      qrbox: {
+        width: "100%",
+        height: "auto",
+      },
+      fps: 5,
+    });
+
+
+    scanner.render(success, error);
+
+    function success(result) {
+      /* scanner.clear(); */
+      setScanResult((scanResult) => result);
+      setShow(show=>'block')
+    }
+
+    function error(err) {
+      console.warn(err);
+    }
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center text-primary">QR-Code Scanner</h1>
+      <div
+        style={{
+          maxWidth: "18rem",
+          display: "block",
+          margin: "auto",
+        }}
+        id="reader"
+      ></div>
+      <br />
+      <div style={{display:show}}>
+        <h3 className="text-center ">Results</h3>
+        <br/>
+        <a  href={scanResult}><h3 className="text-center">{scanResult}</h3></a>
+      </div>
+      <p className="position-absolute bottom-0">&#169;2024 Rojo's Ltd | All Rights Are Reserved</p>
+    </div>
+  );
+}
